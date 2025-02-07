21389759bc9cdb376fbfda29e09c1764ee6d394e
cing <cing@163.com>
2025-02-07 17:08:19 +0800

    02.07

--- Software/Firefox/BackupProfiles/Modules/QD-Common.bat
+++ Software/Firefox/BackupProfiles/Modules/QD-Common.bat
@@ -25,7 +25,6 @@ start  "" "%SoftDir%\PixPin\PixPin.exe"
 start  "" "%SoftDir%\Ditto\Ditto.exe"
 ::start  "" "%SoftDir%\ProcessLassoPro\_Start-ProcessLasso.bat"
 
-
 timeout /t 5 /nobreak
 
 ::启动程序
@@ -35,9 +34,24 @@ start  "" "%SoftDir%\Listary Pro\UserData\Run_listary.bat"
 ::Listary6代
 ::start  "" "%SoftDir%\Listary 6\listary.exe"
 
+::将steamcommunity_302.exe添加到防火墙，再启動程序
+:: 检查是否存在同名的防火墙规则
+netsh advfirewall firewall show rule name=steamcommunity_302.exe >nul 2>&1
+
+if %errorlevel%==0 (
+    echo 防火墙规则 'steamcommunity_302.exe' 已经存在。
+) else (
+    :: 如果不存在，则添加新的防火墙规则
+    netsh advfirewall firewall add rule name="steamcommunity_302.exe" dir=in action=allow program="%SoftDir%\steamcommunity_302\steamcommunity_302.exe" protocol=any
+    if %errorlevel%==0 (
+        echo 成功添加防火墙规则 'steamcommunity_302.exe'.
+    ) else (
+        echo 添加防火墙规则失败！请检查权限或路径是否正确。
+    )
+)
+timeout /t 5 /nobreak
+start  "" "%SoftDir%\steamcommunity_302\steamcommunity_302.exe"
+
 ::管理员启动
 mshta vbscript:createobject("shell.application").shellexecute("""%SoftDir%\RimeIME Portable\weasel\WeaselServer.exe""","::",,"runas",1)(window.close)
 
-timeout /t 60 /nobreak
-
-start  "" "%SoftDir%\steamcommunity_302\steamcommunity_302.exe"
--- Software/Firefox/BackupProfiles/Modules/QD-Home.bat
+++ Software/Firefox/BackupProfiles/Modules/QD-Home.bat
@@ -12,11 +12,16 @@ cd /d %~dp0
 set SoftDir=..\..\..\Software
 
 
-::延迟启动
-start "" "%SoftDir%\..\..\PyBingWallpaper\BingWallpaper.exe"
+::启動Foxmail
 start "" "%SoftDir%\..\..\Tencent\Foxmail\Foxmail.exe"
-start "" "%SoftDir%\..\..\Tencent\Weixin\Weixin.exe"
+REM 等待Foxmail完全启动，可根据需要调整等待时间
+timeout /t 3 /nobreak >nul
+:: 使用PowerShell脚本关闭Foxmail的主窗口但不终止进程
+powershell -command "& {$app = Get-Process -Name Foxmail; if ($app) { $app.CloseMainWindow() | Out-Null } else { Write-Host 'Foxmail is not running.' }}"
 
+::普通启动
+start "" "%SoftDir%\..\..\PyBingWallpaper\BingWallpaper.exe"
+start "" "%SoftDir%\..\..\Tencent\Weixin\Weixin.exe"
 
 ::完成後退出
 exit
