1074ba13760a1ec521ae200a5abf3de2fb810073
玄清介 <dupontjoy@users.noreply.github.com>
2025-09-06 13:17:36 +0800

    update

--- Software/Firefox/user.js
+++ Software/Firefox/user.js
@@ -117,6 +117,7 @@ user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", false);//关闭
 user_pref("intl.icu4x.segmenter.enabled", false);//双击是选取一个短句
 user_pref("extensions.screenshots.disabled", true);//禁用自带截图
 user_pref("layout.css.system-ui.enabled", false);//解決小红书emoji显示错误问题
+user_pref("extensions.webextensions.restrictedDomains", "");//清空禁止的内部页
 
 
 /******************************************************************************************
