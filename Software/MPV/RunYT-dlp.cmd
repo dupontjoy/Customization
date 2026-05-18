fd008966e4a675ede8ab950074062a6e2fe40047
玄清介 <dupontjoy@users.noreply.github.com>
2026-05-18 11:51:45 +0800

    fix: update

--- Software/MPV/RunYT-dlp.cmd
+++ Software/MPV/RunYT-dlp.cmd
@@ -1,4 +1,4 @@
-::2026.03.25
+::2026.05.16
 ::记得保存为ASNI编码
 
 @echo off & setlocal enabledelayedexpansion
@@ -70,7 +70,7 @@ goto :eof
 
 :setting_yt-dlp_params
 ::设置yt-dlp下载参数（使用deno运行环境才能下载youtube视频）
-set "title=%%(title)s@%%(uploader)s.%%(ext)s"
+set "title=%%(title)s%%(upload_date)s@%%(uploader)s.%%(ext)s"
 set yt-dlp_params=--js-runtimes deno --cookies-from-browser firefox:"%firefox_profile%" %format_res% %format_px% --ffmpeg-location %ffmpeg% -o "%title%"
 ::下载视频转换成mp4（用--merge-output-format参数）
 set yt-dlp_download=yt-dlp --merge-output-format mp4 %yt-dlp_params% -P %SaveDir% "%link%"
