ac001a96e58fd5fd7e9d2953b54e9b62b345f9ef
玄清介 <dupontjoy@users.noreply.github.com>
2026-06-24 21:22:38 +0800

    fix: update

--- Software/Firefox/BackupProfiles/Modules/Sync.cmd
+++ Software/Firefox/BackupProfiles/Modules/Sync.cmd
@@ -68,9 +68,9 @@ xcopy "%BackupDir%\..\..\Software\Capslock+\CapsLock+settings.ini" "%OutputDir%\
 xcopy "%BackupDir%\..\..\Software\CCleaner\ccleaner.ini" "%OutputDir%\Customization\Software\CCleaner\" /y
 
 :clashverge£¨CVR£©
-robocopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\profiles" "%OutputDir%\Customization\Software\CVR\profiles" /MIR /ZB /R:3 /W:5
-xcopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\profiles.yaml" "%OutputDir%\Customization\Software\CVR\" /y
-xcopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\verge.yaml" "%OutputDir%\Customization\Software\CVR\" /y
+:robocopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\profiles" "%:::OutputDir%\Customization\Software\CVR\profiles" /MIR /ZB /R:3 /W:5
+:xcopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\profiles.yaml" "%OutputDir%\Customization\Software\CVR\" /y
+:xcopy "C:\Users\%USERNAME%\AppData\Roaming\io.github.clash-verge-rev.clash-verge-rev\verge.yaml" "%OutputDir%\Customization\Software\CVR\" /y
 
 :fab
 xcopy "%BackupDir%\..\..\Software\fab\Firewall App Blocker@Cing.ini" "%OutputDir%\Customization\Software\fab\" /y
