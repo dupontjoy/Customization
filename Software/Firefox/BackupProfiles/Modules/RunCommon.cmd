7ac334900eb1d21038bc9cbdcd5a297d54e05969
玄清介 <dupontjoy@users.noreply.github.com>
2026-07-04 20:47:45 +0800

    fix: update

--- Software/Firefox/BackupProfiles/Modules/RunCommon.cmd
+++ Software/Firefox/BackupProfiles/Modules/RunCommon.cmd
@@ -11,40 +11,8 @@ pushd %~dp0
 ::从批处理所在位置到Software文件夹,共跨了3层
 cd ..\..\..\Software
 
-:delete
-::清理WinSxS，存储了Windows操作系统更新和补丁后的备份文件
-Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
-::删除D:\Temp文件夹
-rd /s /q "D:\Temp"
-
-::删除firefox配置文件夹误生成的cache2文件夹
-rd /s /q "%cd%\..\Profiles\FxProfiles\cache2"
-
-::删除docbox的缓存数据
-rd /s /q "C:\Users\%USERNAME%\AppData\Roaming\DocBox"
-
-::删除calibre的缓存数据
-rd /s /q "C:\Users\%USERNAME%\Calibre 书库\.caltrash"
-
-::删除一些软件的log文件
-rd /s /q "C:\ProgramData\Anytxt\log"
-rd /s /q "C:\ProgramData\Winhance\Logs"
-rd /s /q "C:\ProgramData\Thunder Network\Logs"
-rd /s /q "C:\ProgramData\Nutstore\logs"
-
-::删除ztasker User文件夹中的无用文件
-rd /s /q "%cd%\zTasker\User\Backup"
-rd /s /q "%cd%\zTasker\User\Custom"
-rd /s /q "%cd%\zTasker\User\Logs"
-rd /s /q "%cd%\zTasker\User\TasksBackup"
-rd /s /q "%cd%\zTasker\User\Temp"
-
-::删除FoxmailUpdate文件夹, 有可能导致foxmail无法启动
-rd /s /q "%cd%\..\..\Tencent\Foxmail\FoxmailUpdate"
-
-::清空文件夹，但不会删除文件夹本身
-del /s /q /f "C:\Users\%USERNAME%\Downloads\*"
+:run
 
 :listary
 ::Listary5代
@@ -96,3 +64,37 @@ REM 执行脚本并清理
 cscript //nologo click.vbs
 del click.vbs
 
+:delete
+::删除D:\Temp文件夹
+rd /s /q "D:\Temp"
+
+::删除firefox配置文件夹误生成的cache2文件夹
+rd /s /q "%cd%\..\Profiles\FxProfiles\cache2"
+
+::删除docbox的缓存数据
+rd /s /q "C:\Users\%USERNAME%\AppData\Roaming\DocBox"
+
+::删除calibre的缓存数据
+rd /s /q "C:\Users\%USERNAME%\Calibre 书库\.caltrash"
+
+::删除一些软件的log文件
+rd /s /q "C:\ProgramData\Anytxt\log"
+rd /s /q "C:\ProgramData\Winhance\Logs"
+rd /s /q "C:\ProgramData\Thunder Network\Logs"
+rd /s /q "C:\ProgramData\Nutstore\logs"
+
+::删除ztasker User文件夹中的无用文件
+rd /s /q "%cd%\zTasker\User\Backup"
+rd /s /q "%cd%\zTasker\User\Custom"
+rd /s /q "%cd%\zTasker\User\Logs"
+rd /s /q "%cd%\zTasker\User\TasksBackup"
+rd /s /q "%cd%\zTasker\User\Temp"
+
+::删除FoxmailUpdate文件夹, 有可能导致foxmail无法启动
+rd /s /q "%cd%\..\..\Tencent\Foxmail\FoxmailUpdate"
+
+::清空文件夹，但不会删除文件夹本身
+del /s /q /f "C:\Users\%USERNAME%\Downloads\*"
+
+::清理WinSxS，存储了Windows操作系统更新和补丁后的备份文件
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
